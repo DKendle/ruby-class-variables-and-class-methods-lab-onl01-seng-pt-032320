3dec806e6a64fc2d579bb9c68738ffdcda1efe8d
Dina Kendle <dina.kendle@gmail.com>
2020-04-16 01:00:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,11 +43,12 @@ class Song
     artists_count = {}
       @@artists.each do |artist|
         if artists_count[artist]
-          artists_count[artist] +=1
+          artists_count[artist] += 1
         else 
           artists_count[artist] = 1
         end
-    artists_count
+      end
+      artists_count
   end
   
   
