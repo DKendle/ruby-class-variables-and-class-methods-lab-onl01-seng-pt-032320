22aec1163d1b6f62f27cd1e08057bb1c9edd037e
Dina Kendle <dina.kendle@gmail.com>
2020-04-16 01:01:50 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -20,15 +20,36 @@ class Song
   end
   
   def self.genres
-    @@genres 
-  end
-  def self.artists
-   
-  end
-  def self.genre_count
-    
+    @@genres.uniq
   end
   
+  def self.artists
+   @@artists.uniq
+  end
+  
+  def self.genre_count
+    genre_count = {}
+      @@genres.each do |genre|
+          if genre_count[genre]
+            genre_count[genre] += 1 
+          else
+            genre_count[genre] = 1
+          end
+      end
+    genre_count
+  end
+  
+  def self.artist_count
+    artists_count = {}
+      @@artists.each do |artist|
+        if artists_count[artist]
+          artists_count[artist] += 1
+        else 
+          artists_count[artist] = 1
+        end
+      end
+      artists_count
+  end
   
   
 end
