766ac4632b3ef044bf0b7ddb21fbe2a9528aa36c
Dina Kendle <dina.kendle@gmail.com>
2020-04-16 00:55:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,14 +30,14 @@ class Song
   def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
-    if genre_count[genre]
-        genre_count +=1 
-   else 
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
         genre_count[genre] = 1
-   end
-   genre_count
+      end
+    end
+    genre_count
   end
-  
   
   
 end
